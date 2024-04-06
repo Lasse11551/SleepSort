7861b3c75dd9b21feb5460baa448cf3a6fe1e23c
Xarantaur <115454990+Xarantaur@users.noreply.github.com>
2024-04-06 22:47:12 +0200

    first commit

--- script.js
+++ script.js
@@ -1,37 +1,140 @@
-'use strict';
+"use strict";
 
 // ------------- VIEW ----------------
-window.addEventListener("load", start)
+window.addEventListener("load", start);
+let inputArray = [];
+let sortedArr = [];
+let loadingBars = [];
+
+function addNumber() {
+  event.preventDefault();
+  let inputValue = document.querySelector("#inputNumbers");
+
+  inputArray.push(inputValue.value);
+  inputValue.value = "";
+  console.log("input Array", inputArray);
+  updateArrayContainer();
+}
+
+function updateArrayContainer() {
+  let arrayContainer = document.querySelector(".array-container");
+  arrayContainer.innerHTML = "";
+
+  inputArray.forEach((number) => {
+    let numberElement = document.createElement("span");
+    numberElement.textContent = number + " ";
+    arrayContainer.appendChild(numberElement);
+  });
+}
+
+function sortArray() {
+  sleepSort(inputArray);
+}
+
+function positionNumbers(num) {
+  let sortedArrayContainer = document.querySelector(".sorted-array-container");
+  let numberElement = document.createElement("span");
+  numberElement.textContent = num + " ";
+  sortedArrayContainer.appendChild(numberElement);
+}
+
+function clearArrayInput() {
+  let throwUnsortedContainer = document.querySelector(".array-container");
+  let throwSortedContainer = document.querySelector(".sorted-array-container");
+  throwUnsortedContainer.textContent = "";
+  throwSortedContainer.textContent = "";
+  sortedArr = [];
+  console.log(sortedArr);
+  inputArray = [];
+  console.log(inputArray);
+  console.log("cleared data");
+}
+
+document.querySelector("#clearButton").addEventListener("click", clearArrayInput);
+document.querySelector("#startSort").addEventListener("click", sortArray);
+document.querySelector("#addButton").addEventListener("click", addNumber);
 
 // ------------- Model -----------------
 
-function sleepSort(arr) {
+function createLoadingBars(arr) {
+    let loadingBars = [];
+    let sortedArrayContainer = document.querySelector(".sorted-array-container");
+    sortedArrayContainer.innerHTML = "";
+  
+    for (let num of arr) {
+      let loadingBarContainer = document.createElement("div");
+      loadingBarContainer.classList.add("loading-bar-container");
+      let loadingBar = document.createElement("div");
+      loadingBar.classList.add("loading-bar");
+      loadingBarContainer.appendChild(loadingBar);
+      sortedArrayContainer.appendChild(loadingBarContainer);
+      let valueTextNode = document.createTextNode(num);
+      
+      loadingBar.appendChild(valueTextNode);
+      loadingBarContainer.appendChild(loadingBar);
+      sortedArrayContainer.appendChild(loadingBarContainer);
+      loadingBars.push(loadingBar);
+    }
+  
+    return loadingBars;
+  }
+  
+  function animateLoadingBar(loadingBar, totalTime, callback) {
+    let startTime = performance.now();
+    
+    function updateAnimation() {
+      let currentTime = performance.now();
+      let elapsedTime = currentTime - startTime;
+      let percentage = Math.max(0, Math.min(100, 100 * (1 - elapsedTime / totalTime)));
+      loadingBar.style.width = percentage + "%";
+      if (percentage > 0) {
+        requestAnimationFrame(updateAnimation);
+      } else {
+        callback();
+      }
+    }
+    
+    requestAnimationFrame(updateAnimation);
+  }
+  
+  function sleepSort(arr) {
     // Create an array to hold promises
     let promises = [];
- 
-    for (let num of arr) {
-        // For each number, create a new promise that resolves after 'num' milliseconds
-        let promise = new Promise((resolve) => {
-            setTimeout(() => {
-                console.log(num);  // Print the number after 'num' milliseconds
-                resolve();
-            }, num);
+    let sortedArr = []; // Use local sortedArr
+    let loadingBars = createLoadingBars(arr);
+  
+    for (let i = 0; i < arr.length; i++) {
+      let num = arr[i];
+      let totalTime = num * 1000;
+  
+      // For each number, create a new promise that resolves after 'num' milliseconds
+      let promise = new Promise((resolve) => {
+        animateLoadingBar(loadingBars[i], totalTime, () => {
+          sortedArr.push(num);
+          resolve();
+          positionNumbers(num);
         });
- 
-        // Add the promise to the array
-        promises.push(promise);
+      });
+  
+      // Add the promise to the array
+      promises.push(promise);
     }
- 
+  
     // Wait for all promises to resolve
-    Promise.all(promises).catch((error) => console.error(error));
+    Promise.all(promises)
+      .then(() => {
+        console.log("Sorted Array:", sortedArr);
+      })
+      .catch((error) => console.error(error));
 }
- 
+
+
+  
+
 // Test the sleepSort function
-let arr = [34, 23, 122, 9];
-sleepSort(arr);
+/* let arr = [34, 23, 122, 9];
+console.log(arr) */
 
 // ------------- CONTROLLER --------------
 
-function start() {
-
-}
+function start() {}
